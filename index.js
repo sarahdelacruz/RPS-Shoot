bc1fde4963588054a00f87cf41e44676af6ad1c3
sarahdelacruz <71873629+sarahdelacruz@users.noreply.github.com>
2021-05-05 13:58:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,26 +17,25 @@ var title = document.querySelector('h1');
 var player1 = document.querySelector('.p1');
 var player2 = document.querySelector('.p2');
 
-//identical image
-if (randomPic1 == 1 & randomPic2 == 1 | randomPic1 == 2 & randomPic2 == 2 | randomPic1 == 3 & randomPic2 == 3){
-		 title.textContent = 'Try Again.'};
-
-//Wins
-if (randomPic1 == 2 & randomPic2 == 1 | randomPic1 == 1 & randomPic2 == 2 )	{
+//winners
+if (randomPic1 == 2 & randomPic2 == 1 | randomPic1 == 1 & randomPic2 == 2 ){
 		 title.textContent = 'Paper Wins!'};
-if (randomPic1 == 3 & randomPic2 == 1 | randomPic1 == 1 & randomPic2 == 3 )	{
+if (randomPic1 == 3 & randomPic2 == 1 | randomPic1 == 1 & randomPic2 == 3 ){
 		 title.textContent = 'Rock Wins!'};
-if (randomPic1 == 2 & randomPic2 == 3 | randomPic1 == 3 & randomPic2 == 2 )	{
+if (randomPic1 == 2 & randomPic2 == 3 | randomPic1 == 3 & randomPic2 == 2 ){
 		 title.textContent = 'Scissor Wins!'};
 
-//player wins n loss
-if (randomPic1 == 2 & randomPic2 == 1 | randomPic1 == 1 & randomPic2 == 3 | randomPic1 == 3 & randomPic2 == 2) {
+//player winners and losers
+if (randomPic1 == 2 & randomPic2 == 1 | randomPic1 == 1 & randomPic2 == 3 | randomPic1 == 3 & randomPic2 == 2){
 	player1.innerHTML = '&#127942; <b>Player 1 Wins</b>'};
-if (randomPic2 == 2 & randomPic1 == 1 | randomPic1 == 3 & randomPic2 == 1 | randomPic1 == 2 & randomPic2 == 3) {
+if (randomPic2 == 2 & randomPic1 == 1 | randomPic1 == 3 & randomPic2 == 1 | randomPic1 == 2 & randomPic2 == 3){
 	player2.innerHTML = '&#127942; <b>Player 2 Wins</b>'};
-if (randomPic1 == 1 & randomPic2 == 2 | randomPic1 == 3 & randomPic2 == 1 | randomPic1 == 2 & randomPic2 == 3) {
+if (randomPic1 == 1 & randomPic2 == 2 | randomPic1 == 3 & randomPic2 == 1 | randomPic1 == 2 & randomPic2 == 3){
 	player1.innerHTML = '&#10060; <strike>Player 1</strike>'}
-if (randomPic1 == 2 & randomPic2 == 1 | randomPic1 == 1 & randomPic2 == 3 | randomPic1 == 3 & randomPic2 == 2) {
+if (randomPic1 == 2 & randomPic2 == 1 | randomPic1 == 1 & randomPic2 == 3 | randomPic1 == 3 & randomPic2 == 2){
 	player2.innerHTML = '&#10060; <strike>Player 2</strike>'}
 
+//for identical image
+if (randomPic1 == 1 & randomPic2 == 1 | randomPic1 == 2 & randomPic2 == 2 | randomPic1 == 3 & randomPic2 == 3){
+		 title.textContent = 'Try Again.'};
 //this is the end of this code 
